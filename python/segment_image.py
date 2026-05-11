87d221e5cea4b37c780d86828426c3aadc15488a
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-05-11 12:32:40 +0100

    add argparse for input and output path

--- python/segment_image.py
+++ python/segment_image.py
@@ -1,4 +1,5 @@
 import imageio.v3 as iio3
+import argparse
 import numpy as np
 from skimage.filters import gaussian, threshold_li
 from skimage.measure import label
@@ -7,30 +8,39 @@ from skimage.morphology import remove_small_objects
 from skimage.feature import peak_local_max
 from scipy import ndimage as ndi
 import matplotlib.pyplot as plt
+from pathlib import Path
 
-filename = "data/C1-images/C1-Rep1_DIV3_Cortical_r02c04f01-1.tiff"
-image = iio3.imread(filename)
+def segment_image(input_path: Path, output_path: Path) -> None:
+    image = iio3.imread(input_path)
 
-# gaussian blur
-blurred_image = gaussian(image, sigma=3)
+    # gaussian blur
+    blurred_image = gaussian(image, sigma=3)
 
-# threshold 
-threshold = threshold_li(blurred_image)
-thresholded_image = blurred_image > threshold
+    # threshold 
+    threshold = threshold_li(blurred_image)
+    thresholded_image = blurred_image > threshold
 
-# watershed to split items?
-distance = ndi.distance_transform_edt(thresholded_image)
-coords = peak_local_max(distance, footprint=np.ones((3, 3)), labels=thresholded_image)
-nuclei = watershed(-distance, mask=thresholded_image)
+    # watershed to split items?
+    distance = ndi.distance_transform_edt(thresholded_image)
+    nuclei = watershed(-distance, mask=thresholded_image)
 
-# connected components
-nuclei = label(thresholded_image)
+    # connected components
+    nuclei = label(thresholded_image)
 
-# filter small items
-nuclei = remove_small_objects(nuclei, max_size=100)
+    # filter small items
+    nuclei = remove_small_objects(nuclei, max_size=100)
 
-iio3.imwrite(path, nuclei)
-
-
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    iio3.imwrite(output_path, nuclei)
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_path", help="path of the input image")
+    parser.add_argument("output_path", help="path of the output image")
+    args = parser.parse_args()
+
+    segment_image(Path(args.input_path), Path(args.output_path))
+
+if __name__ == "__main__":
+    main()
