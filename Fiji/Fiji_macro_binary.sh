79d71fef9f709fe47a795b841635d11eb6796cab
Stefania Marcotti <33655510+stemarcotti@users.noreply.github.com>
2026-06-09 11:53:37 +0100

    add bash script and Fiji macro for binary masks

---
+++ Fiji/Fiji_macro_binary.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Run Fiji macro headless (binary mask nuclei)
+#SBATCH --job-name=macro-binary
+#SBATCH --ntasks=1
+#SBATCH --time=01:00:00
+#SBATCH --partition=ncpu
+#SBATCH --mem=256G    
+#SBATCH --threads-per-core=1    
+#SBATCH --cpus-per-task=4 
+ 
+cd /nemo/stp/lm/working/marcots/Fiji.app
+./ImageJ-linux64 --headless --console -macro ../S-BIAD7/binary_nuclei_macro.ijm 
+
