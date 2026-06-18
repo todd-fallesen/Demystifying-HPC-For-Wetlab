dee685e94f05dd63c34bf862cd88db7e28842792
ruaridhg <32329546+ruaridhg@users.noreply.github.com>
2026-06-18 14:43:15 +0100

    Apply suggestions from code review
    Co-authored-by: Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>

--- python/python_seg_task_script.sh
+++ python/python_seg_task_script.sh
@@ -8,19 +8,18 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mail-user=todd.fallesen@crick.ac.uk
-#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=/camp/home/fallest/working/fallest/CPOut/slurm-test.out
-#SBATCH --error=/camp/home/fallest/working/fallest/CPOut/slurm-test.err
+#SBATCH --output=logs/slurm.%x.%N.%j.out
+#SBATCH --error=logs/slurm.%x.%N.%j.err
 
 
 
-source $HOME/.bashrc
 ml Anaconda3
 conda activate scikit-image-env
 
 echo $1
-outdir=$(basename $1)
-python "$HOME/HPC_Paper/python/segment_image.py" "${1}/" "${outdir}_output.tiff"
+filename=$(basename $1 .tiff)
+python "$HOME/HPC_Paper/python/segment_image.py" "${1}/" "${filename}_output.tiff"
 
 
 
