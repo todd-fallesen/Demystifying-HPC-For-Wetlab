fa559f95d7952db54d279a841053916cb1354a4a
Todd Fallesen <49306167+todd-fallesen@users.noreply.github.com>
2026-06-30 13:01:50 +0100

    added comments

--- Fiji/Fiji_macro_binary.sh
+++ Fiji/Fiji_macro_binary.sh
@@ -8,6 +8,8 @@
 #SBATCH --threads-per-core=1    
 #SBATCH --cpus-per-task=4 
  
+
+ #Specifiy in the path below where Fiji lives in your installation. If you use a module based FIJI, you could use module load
 cd /nemo/stp/lm/working/marcots/Fiji.app
 ./ImageJ-linux64 --headless --console -macro ../S-BIAD7/binary_nuclei_macro.ijm 
 
